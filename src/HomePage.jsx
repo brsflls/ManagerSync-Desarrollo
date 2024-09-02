7637b6264fdf8c5471648c9613f6c11ec1472a15
Brittany Fallas <103863271+brsflls@users.noreply.github.com>
2024-09-02 13:34:48 -0600

    First commit
    Primer commit. Creación del proyecto con tailwind para react

---
+++ src/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css'
+
+
+export function  HomePage() {
+
+  return ( 
+      <h1 className="font-bold">
+        <span className='text-4xl'>Manager</span>
+        <span className='text-4xl text-blue-400'>Sync</span>
+      </h1>
+
+       )
+}
