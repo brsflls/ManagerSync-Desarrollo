7b9a8b4c6fc9bde96138778b5b5a578b013ebf67
D4r2k0Ursoth <alessandro-arce@hotmail.com>
2024-09-03 19:49:13 -0600

    Añadi nuevo moudulo de loading page ademas de los componenentes que lo conforman concluyo con uno de  mis tickets para esta semana saludos!!!!

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -4,6 +4,7 @@ import { Register } from "./components/activity/Register";
 import{Forgot_pass}from "./components/activity/Forgot_pass";
 import{Mantenimiento_productos}from "./components/activity/Mantenimiento_productos";
 import './index.css'
+import { LoadingPage } from '././LoadingPage.jsx';
 
 
 export function  HomePage() {
@@ -11,12 +12,14 @@ export function  HomePage() {
   return (
     <div className="container bg-blue-100  w-screen h-screen " >
 
-{/*<Login /> <Login />  <Register />
-     <Forgot_pass/><Login /> 
+{/*<Login /> 
+<Login /> 
+ <Register />
+      <Forgot_pass/>
+     <Login /> 
+<  Mantenimiento_productos/> 
 */}
-   
-   <  Mantenimiento_productos/>
-      
+   <LoadingPage/>
     </div>
   );
 }
---
+++ src/components/activity/ButtonLoadPage.jsx
@@ -0,0 +1,16 @@
+import "../../index.css";
+
+
+export function ButtonLoadingPage() {
+
+  return (
+      <div className="mt-5">
+      <button>
+      <button className=" text-2xl bg-blue-950 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded-lg">
+        Empezar
+      </button>
+      </button>
+      </div>
+  );
+}
+
