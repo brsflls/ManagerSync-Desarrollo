7d74f1b548df046288d17edb5215360f997e8d60
D4r2k0Ursoth <alessandro-arce@hotmail.com>
2024-09-08 22:40:08 -0600

    Add act

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,35 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
+import {Link} from "react-router-dom"
 import { ReactSVG } from 'react-svg';
-import "../../dev/css/components/_header.css"; // Si necesitas mantener tus estilos personalizados
-
+import "../../dev/css/components/_header.css"
 export const Header = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false)
+    
+    return (
+    <nav>
+            <Link to="/" className="logo">
+            <ReactSVG src="src/assets/logo-completo.svg" />
 
-  return (
-    <nav className="bg-white    w-screen h-max  ">
-      <div className="w-full  h-max  flex justify-between  ">
-        <Link to="/" className="logo">
-          <ReactSVG src="src/assets/logo-completo.svg" className="w-36" />
-        </Link>
-        {/* Menu button for mobile */}
-        <div 
-          className="menu  md:hidden cursor-pointer"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          <span className="block w-8 h-1 bg-white mb-1"></span>
-          <span className="block w-8 h-1 bg-white mb-1"></span>
-          <span className="block w-8 h-1 bg-white"></span>
-        </div>
-        {/* Navigation Links */}
-        <ul className={`md:flex-row md:space-x-8 md:items-center absolute md:static top-16  w-full md:w-auto bg-white md:bg-transparent z-10 transform ${menuOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out`}>
-          <li className="my-2 md:my-0 text-center">
-            <Link to="/Soluciones" className="hover:text-yellow-500">Soluciones</Link>
-          </li>
-          <li className="my-2 md:my-0 text-center">
-            <Link to="/" className="hover:text-yellow-500">Acerca de</Link>
-          </li>
-          <li className="my-2 md:my-0 text-center">
-            <Link to="/LogIn" className="hover:text-yellow-500">Iniciar sesión</Link>
-          </li>
-          <li className="my-2 md:my-0 text-center">
-            <Link to="/Registro" className="hover:text-yellow-500">Registrarse</Link>
-          </li>
+            </Link>
+            <div className="menu" onClick={()=>{setMenuOpen(!menuOpen)}}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        <ul className={menuOpen ? "open" : ""}>
+            <li>
+                <Link to="/Soluciones">Soluciones</Link>
+            </li>
+            <li>
+                <Link to="/Acercade">Acerca de</Link>
+            </li>
+            <li>
+                <Link to="/Login">Iniciar sesión</Link>
+            </li>
+            <li>
+                <Link to="/Register">Registrarse</Link>
+            </li>
+
         </ul>
-      </div>
-    </nav>
-  );
-};
+    </nav>)
+}
